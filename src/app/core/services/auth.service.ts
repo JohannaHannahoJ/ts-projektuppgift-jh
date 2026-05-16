fb3c07ea38d2f255c2573282b5206e666b1514a7
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-16 09:35:48 +0200

    Add register functionality

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
+import { RegisterResponse } from '../models/register-response';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {}
+export class AuthService {
+    private http = inject(HttpClient); // används för http-anrop till backend
+    url: string = "http://localhost:3000"
+
+  register(user: User): Observable<RegisterResponse> {
+    // User skickas med i request body, svaret typas som RegisterResponse
+    return this.http.post<RegisterResponse>(this.url + "/users/register", user);
+  }
+}
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { RegisterResponse } from '../../core/models/register-response';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  username: string = "";
+  password: string = "";
+
+  message = signal(""); // För meddelanden i frontend
+  authService = inject(AuthService); // används för att kunna anropa backends auth routes
+  router = inject(Router);
+
+  // körs när anv. skickar formuläret
+  register(): void {
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.register(user).subscribe({
+      next: (response: RegisterResponse) => {
+        localStorage.setItem("flashMessage", response.message);
+        this.router.navigate(["/login"]);
+      },
+
+      error: (error) => {
+        this.message.set(error.error.message)
+      }
+    });
+  }
+}
