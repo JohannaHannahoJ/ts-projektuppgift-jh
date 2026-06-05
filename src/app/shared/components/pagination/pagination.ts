41d19329b32c230271e7211a7e32cc252a700bd3
JohannaHannahoJ <joha2102@student.miun.se>
2026-06-05 10:16:59 +0200

    Add pagination

--- src/app/shared/components/pagination/pagination.ts
+++ src/app/shared/components/pagination/pagination.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,4 +6,32 @@ import { Component } from '@angular/core';
   templateUrl: './pagination.html',
   styleUrl: './pagination.css',
 })
-export class Pagination {}
+export class Pagination {
+  // aktuell sida, hämtas från parent (courses)
+  @Input() currentPage = 1;
+  // antal sidor hämtas från parent
+  @Input() pageCount = 1;
+  // skickar tillbaka vald sida till parent
+  @Output() pageChange = new EventEmitter<number>();
+
+  // gå till första sid
+  firstPage() {
+    this.pageChange.emit(1);
+  }
+  // gå tillbaka en sida, stopp sid 1
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.pageChange.emit(this.currentPage - 1);
+    }
+  }
+  // går fram en sida, max till sista
+  nextPage() {
+    if (this.currentPage < this.pageCount) {
+      this.pageChange.emit(this.currentPage + 1);
+    }
+  }
+  // gå till sista sidan av arrayen
+  lastPage() {
+    this.pageChange.emit(this.pageCount);
+  }
+}
